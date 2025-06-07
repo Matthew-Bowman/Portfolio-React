3b29f6ed46f5944a5a5c44c927d16520ac52cfd9
Matthew <matthew.bowman@tutanota.com>
2025-06-07 10:25:43 +0100

    Installed & Implemented vite-plugin-beasties

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { beasties } from "vite-plugin-beasties";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    beasties({
+      // Plugin options
+      options: {
+        // Beasties library options
+        preload: "swap",
+        pruneSource: true, // Enable pruning CSS files
+        inlineThreshold: 4000, // Inline stylesheets smaller than 4kb
+      },
+      // Filter to apply beasties only to specific HTML files
+      filter: (path) => path.endsWith(".html"),
+    }),
+  ],
+});
