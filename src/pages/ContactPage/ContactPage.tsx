614c46668f823b6e2bced6ca91a9a54f879aa142
Matthew <matthew.bowman@tutanota.com>
2025-06-18 23:39:18 +0100

    Integrated react-helmet on pages (inc. canonical for Google Search)

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,5 +1,7 @@
 import { useState, type FormEvent } from 'react';
 import styles from './ContactPage.module.css'
+import { Helmet } from 'react-helmet';
+import { ROUTES } from '../../data/routes';
 
 function ContactPage() {
 
@@ -35,36 +37,46 @@ function ContactPage() {
   };
 
   return (
-    <section className={styles.contactSection}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Let's Work Together</h1>
-        <p className={styles.subtitle}>Have a project in mind? Drop me a message!</p>
+    <>
+      <Helmet>
+        <link
+          rel="canonical"
+          href={`https://matthewbowman.uk/${ROUTES.ContactUs.path}`}
+        />
+        <title>Contact Us | Matthew Bowman</title>
+      </Helmet>
 
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <div className={styles.inputGroup}>
-            <label>Name</label>
-            <input name='name' type="text" placeholder="Your Name" onChange={handleChange} required />
-          </div>
+      <section className={styles.contactSection}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>Let's Work Together</h1>
+          <p className={styles.subtitle}>Have a project in mind? Drop me a message!</p>
 
-          <div className={styles.inputGroup}>
-            <label>Email</label>
-            <input name='email' type="email" placeholder="your@email.com" onChange={handleChange} required />
-          </div>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <div className={styles.inputGroup}>
+              <label>Name</label>
+              <input name='name' type="text" placeholder="Your Name" onChange={handleChange} required />
+            </div>
 
-          <div className={styles.inputGroup}>
-            <label>Message</label>
-            <textarea name='message' rows={5} placeholder="Tell me about your project..." onChange={handleChange} required />
-          </div>
+            <div className={styles.inputGroup}>
+              <label>Email</label>
+              <input name='email' type="email" placeholder="your@email.com" onChange={handleChange} required />
+            </div>
 
-          {/* Honeypot Field to Prevent Abuse (Bots will fill this out automatically) */}
-          <input name='honeypot' type="text" hidden />
+            <div className={styles.inputGroup}>
+              <label>Message</label>
+              <textarea name='message' rows={5} placeholder="Tell me about your project..." onChange={handleChange} required />
+            </div>
 
-          <button type="submit" className={styles.submitButton}>Send Message</button>
-        </form>
+            {/* Honeypot Field to Prevent Abuse (Bots will fill this out automatically) */}
+            <input name='honeypot' type="text" hidden />
 
-        {status && <p style={{ marginTop: '1rem', textAlign: 'center' }}>{status}</p>}
-      </div>
-    </section>
+            <button type="submit" className={styles.submitButton}>Send Message</button>
+          </form>
+
+          {status && <p style={{ marginTop: '1rem', textAlign: 'center' }}>{status}</p>}
+        </div>
+      </section>
+    </>
   );
 }
 
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -3,6 +3,7 @@ import heroIllustration from '../../assets/hero-illustration.svg'
 import Button from '../../components/Button/Button';
 import styles from './LandingPage.module.css'
 import { ROUTES } from '../../data/routes';
+import { Helmet } from 'react-helmet';
 
 function LandingPage() {
 
@@ -13,23 +14,33 @@ function LandingPage() {
   }
 
   return (
-    // Hero
-    <div className={styles.sections}>
-      <div className={styles.sectionLeft}>
+    <>
+      <Helmet>
+        <link
+          rel="canonical"
+          href={`https://matthewbowman.uk/${ROUTES.Home.path}`}
+        />
+        <title>Home | Matthew Bowman</title>
+      </Helmet>
+      {/* Hero */}
 
-        <h1 className={`${styles.fadeSlideInUp} ${styles.title}`}>Matthew <span>Bowman</span></h1>
-        <h2 className={`${styles.fadeSlideInUp} ${styles.subtitle}`}>Web Developer</h2>
-        <p className={`${styles.fadeSlideInUp} ${styles.slogan}`}>Deploying <span>Dreams</span>, Not Just Code</p>
-        <Button className={`${styles.fadeSlideInUp} ${styles.button}`} text='Contact Me' onClick={handleContactButtonClicked} />
+      <div className={styles.sections}>
+        <div className={styles.sectionLeft}>
 
+          <h1 className={`${styles.fadeSlideInUp} ${styles.title}`}>Matthew <span>Bowman</span></h1>
+          <h2 className={`${styles.fadeSlideInUp} ${styles.subtitle}`}>Web Developer</h2>
+          <p className={`${styles.fadeSlideInUp} ${styles.slogan}`}>Deploying <span>Dreams</span>, Not Just Code</p>
+          <Button className={`${styles.fadeSlideInUp} ${styles.button}`} text='Contact Me' onClick={handleContactButtonClicked} />
+
+        </div>
+
+        <div className={styles.sectionRight}>
+          <img src={heroIllustration} alt='Hero Illustration' className={`${styles.fadeSlideInLeft} ${styles.mainIllustration}`} />
+        </div>
       </div>
 
-      <div className={styles.sectionRight}>
-        <img src={heroIllustration} alt='Hero Illustration' className={`${styles.fadeSlideInLeft} ${styles.mainIllustration}`} />
-      </div>
-    </div>
-
-    // Page Content
+      {/* Page Content */}
+    </>
   );
 }
 
