371592ba21381f6724f323762fc6921989d5ffda
Matthew <matthew.bowman@tutanota.com>
2025-06-27 22:14:57 +0100

    Updated Privacy Policy (Removed Bootstrap)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import styles from './Button.module.css'
 type ButtonProps = {
     className?: string;
     text: string;
-    onClick: () => void; // optional predefined size prop
+    onClick: () => void;
 };
 const Button: FC<ButtonProps> = ({ className, text, onClick }) => {
     return (
